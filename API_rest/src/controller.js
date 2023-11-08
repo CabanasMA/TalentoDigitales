6bacc10fdbf10970561ba3c910458e293b4c933b
CabanasMA <147351330+CabanasMA@users.noreply.github.com>
2023-11-08 13:21:08 -0300

    finalizado api-rest

--- API_rest/src/controller.js
+++ API_rest/src/controller.js
@@ -6,6 +6,25 @@ class PersonaController{
         const [result] = await pool.query(`SELECT * FROM biblioteca`);
         res.json(result);
     }
+
+    async add(req,res){
+        const biblioteca = req.body;
+        const [result] = await pool.query (`INSERT INTO biblioteca(titulo,autor,fecha,ISBN) VALUES(?,?,?,?)`,[biblioteca.titulo,biblioteca.autor,biblioteca.fecha,biblioteca.ISBN]);
+        res.json({"Id insertado": result,insertId});
+    }
+
+    async delete(req,res){
+        const biblioteca = req.body;
+        const [result]= await pool.query (`DELETE FROM biblioteca WHERE id=(?)`,[biblioteca.id]);
+        res.json({"Registros eliminados": result.affectedRows});
+
+    }
+
+    async update(req,res){
+        const biblioteca = req.body;
+        const[result] = await pool.query(`UPDATE biblioteca SET id = (?), titulo = (?), autor = (?), fecha = (?), ISBN = (?)`,[biblioteca.id,biblioteca.titulo,biblioteca.autor,biblioteca.fecha,biblioteca.ISBN]);
+        res.json({" Registros actualizados": result.changedRows});
+    }
 }
 
 export const bibliotecas = new PersonaController();
--- API_rest/src/routes.js
+++ API_rest/src/routes.js
@@ -4,3 +4,9 @@ import { bibliotecas } from "./controller.js";
 export const router = Router()
 
 router.get('/biblioteca', bibliotecas.getAll);
+
+router.post('/biblioteca',bibliotecas.add);
+
+router.delete('/biblioteca',bibliotecas.delete);
+
+router.put('/biblioteca', bibliotecas.update);
